73f0a4ba1d8c894119b3d785c7bafd3cc8901457
Frederick Thomas <fthomas@csedept.uhhilo>
2017-11-28 18:14:44 -1000

    first commit

---
+++ app/src/main/java/nothing/schreiberproject/Database.java
@@ -0,0 +1,13 @@
+package nothing.schreiberproject;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by fthomas on 11/28/2017.
+ */
+
+public class Database extends SQLiteOpenHelper {
+
+}
