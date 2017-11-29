32df32e44b4ca2249cb217cecc5fa40a3337910e
Frederick Thomas <fthomas@csedept.uhhilo>
2017-11-28 19:11:07 -1000

    second commit

---
+++ app/src/main/java/nothing/schreiberproject/Checkout_Page.java
@@ -0,0 +1,13 @@
+package nothing.schreiberproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Checkout_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout__page);
+    }
+}
